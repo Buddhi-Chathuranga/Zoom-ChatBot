a6d3a1c9d227305d6cb5eb4d703364bccd1a5268
Buddhi <53532224+Buddhi-Chathuranga@users.noreply.github.com>
2022-03-11 02:05:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -36,74 +36,75 @@ app.get('/', (req, res) => {
 
 
 
-// app.get('/test/:msg', (req, res) => {
-//   var trigger = [
-//     ["hi","hey","hello"], 
-//     ["how are you", "how is life", "how are things"],
-//     ["what are you doing", "what is going on"],
-//     ["how old are you"],
-//     ["who are you", "are you human", "are you bot", "are you human or bot"],
-//     ["who created you", "who made you"],
-//     ["your name please",  "your name", "may i know your name", "what is your name"],
-//     ["i love you"],
-//     ["happy", "good"],
-//     ["bad", "bored", "tired"],
-//     ["help me", "tell me story", "tell me joke"],
-//     ["ah", "yes", "ok", "okay", "nice", "thanks", "thank you"],
-//     ["bye", "good bye", "goodbye", "see you later"]
-//   ];
+app.get('/test/:msg', (req, res) => {
+  var trigger = [
+    ["hi","hey","hello"], 
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    ["your name please",  "your name", "may i know your name", "what is your name"],
+    ["i love you"],
+    ["happy", "good"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice", "thanks", "thank you"],
+    ["bye", "good bye", "goodbye", "see you later"]
+  ];
 
-//   var reply = [
-//     ["Hi","Hey","Hello"], 
-//     ["Fine", "Pretty well", "Fantastic"],
-//     ["Nothing much", "About to go to sleep", "Can you guest?", "I don't know actually"],
-//     ["I am 1 day old"],
-//     ["I am just a bot", "I am a bot. What are you?"],
-//     ["Buddhi Chathuranga", "My God"],
-//     ["I am nameless", "I don't have a name"],
-//     ["I love you too", "Me too"],
-//     ["Have you ever felt bad?", "Glad to hear it"],
-//     ["Why?", "Why? You shouldn't!", "Try watching TV"],
-//     ["I will", "What about?"],
-//     ["Tell me a story", "Tell me a joke", "Tell me about yourself", "You are welcome"],
-//     ["Bye", "Goodbye", "See you later"]
-//   ];
+  var reply = [
+    ["Hi","Hey","Hello"], 
+    ["Fine", "Pretty well", "Fantastic"],
+    ["Nothing much", "About to go to sleep", "Can you guest?", "I don't know actually"],
+    ["I am 1 day old"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["Buddhi Chathuranga", "My God"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["I will", "What about?"],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself", "You are welcome"],
+    ["Bye", "Goodbye", "See you later"]
+  ];
 
-//   var alternative = ["Haha...", "Eh..."];
+  var alternative = ["Haha...", "Eh..."];
 
-//   function output(input){
-//     try{
-//       var product = input + "=" + eval(input);
-//     } catch(e){
-//       var text = (input.toLowerCase()).replace(/[^\w\s\d]/gi, ""); //remove all chars except words, space and 
-//       text = text.replace(/ a /g, " ").replace(/i feel /g, "").replace(/whats/g, "what is").replace(/please /g, "").replace(/ please/g, "");
-//       if(compare(trigger, reply, text)){
-//         var product = compare(trigger, reply, text);
-//       } else {
-//         var product = alternative[Math.floor(Math.random()*alternative.length)];
-//       }
-//     }
+  function output(input){
+    try{
+      var product = input + "=" + eval(input);
+    } catch(e){
+      var text = (input.toLowerCase()).replace(/[^\w\s\d]/gi, ""); //remove all chars except words, space and 
+      text = text.replace(/ a /g, " ").replace(/i feel /g, "").replace(/whats/g, "what is").replace(/please /g, "").replace(/ please/g, "");
+      if(compare(trigger, reply, text)){
+        var product = compare(trigger, reply, text);
+      } else {
+        var product = alternative[Math.floor(Math.random()*alternative.length)];
+      }
+    }
 
-//     res.send(product);
+    res.send(product);
     
-//   }
-//   function compare(arr, array, string){
-//     var item;
-//     for(var x=0; x<arr.length; x++){
-//       for(var y=0; y<array.length; y++){
-//         if(arr[x][y] == string){
-//           items = array[x];
-//           item =  items[Math.floor(Math.random()*items.length)];
-//         }
-//       }
-//     }
-//     return item;
-//   }
-//   let mm =req.params.msg
-//   let mmm = output(mm)
-//   res.send('Output is ')
+  }
+  function compare(arr, array, string){
+    var item;
+    for(var x=0; x<arr.length; x++){
+      for(var y=0; y<array.length; y++){
+        if(arr[x][y] == string){
+          items = array[x];
+          item =  items[Math.floor(Math.random()*items.length)];
+        }
+      }
+    }
+    return item;
+  }
+  let mm =req.params.msg
+  let x = {firstName:"Johkkn"}; 
+  x.firstName = output(mm)
+  res.send(x.firstName)
   
-// })
+})
 
 
 app.get('/authorize', (req, res) => {
@@ -221,7 +222,9 @@ app.post('/unsplash', (req, res) => {
   }
   //'text': 'You sent ' + req.body.payload.cmd + '     Replay =' + msg
   function sendChat (chatbotToken) {
-    var msgg = String(output(req.body.payload.cmd))
+    var msgg = req.body.payload.cmd
+    let x = {firstName:"Johkkn"}; 
+    x.firstName = output(mm)
     request({
       url: 'https://api.zoom.us/v2/im/chat/messages',
       method: 'POST',
@@ -236,7 +239,7 @@ app.post('/unsplash', (req, res) => {
           },
           'body': [{
             'type': 'message',
-            'text':  'This ' + typeof msgg
+            'text':  'This ' + x.firstName
           }]
         }
       },
