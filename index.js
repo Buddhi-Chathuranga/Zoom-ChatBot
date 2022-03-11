63f0afb0d7d66630da657d44d4c6c6cd8922e2d3
Buddhi <53532224+Buddhi-Chathuranga@users.noreply.github.com>
2022-03-11 05:21:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -241,7 +241,7 @@ app.post('/unsplash', (req, res) => {
           },
           'body': [{
             'type': 'message',
-            'text':  'This ' + obj
+            'text':  'This ' + String(obj)
           }]
         }
       },
