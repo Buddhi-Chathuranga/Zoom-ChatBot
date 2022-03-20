de3092f5c750d5c57c7160184a90e5b9598bd892
IT19149936_Buddhi <it19149936@my.sliit.lk>
2022-03-20 15:01:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,70 +39,110 @@ app.get('/', (req, res) => {
 
 app.get('/test/:msg', (req, res) => {
   const trigger = [
-    ["hi", "hey", "hello"],
+    ["hi", "hey", "hello", "good morning", "good afternoon"],
     ["how are you", "how is life", "how are things"],
-    ["what are you doing", "what is going on"],
+    ["what are you doing", "what is going on", "what is up"],
     ["how old are you"],
     ["who are you", "are you human", "are you bot", "are you human or bot"],
     ["who created you", "who made you"],
-    ["your name please", "your name", "may i know your name", "what is your name"],
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself"
+    ],
     ["i love you"],
-    ["happy", "good"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
     ["bad", "bored", "tired"],
     ["help me", "tell me story", "tell me joke"],
-    ["ah", "yes", "ok", "okay", "nice", "thanks", "thank you"],
-    ["bye", "good bye", "goodbye", "see you later"]
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["thanks", "thank you"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"]
   ];
-
+  
+  // These are bot responses, paired in order with the above 'trigger' phrases
+  
   const reply = [
-    ["Hi", "Hey", "Hello"],
-    ["Fine", "Pretty well", "Fantastic"],
-    ["Nothing much", "About to go to sleep", "Can you guest?", "I don't know actually"],
-    ["I am 1 day old"],
+    ["Hello!", "Hi!", "Hey!", "Hi there!"],
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually"
+    ],
+    ["I am infinite"],
     ["I am just a bot", "I am a bot. What are you?"],
-    ["Buddhi Chathuranga", "My God"],
+    ["The one true God, JavaScript"],
     ["I am nameless", "I don't have a name"],
     ["I love you too", "Me too"],
     ["Have you ever felt bad?", "Glad to hear it"],
     ["Why?", "Why? You shouldn't!", "Try watching TV"],
-    ["I will", "What about?"],
-    ["Tell me a story", "Tell me a joke", "Tell me about yourself", "You are welcome"],
-    ["Bye", "Goodbye", "See you later"]
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["You're welcome"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Yes?"]
+  ];
+  
+  // This is a small set of basically random 'catch alls' for anything that the user enters outside of the possible trigger phrases
+  
+  const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening..."
   ];
 
-  const alternative = ["Haha...", "Eh..."];
-  let product;
-  function output(input){
-    
-    
-    let text = (input.toLowerCase()).replace(/[^\w\s\d]/gi, ""); //remove all chars except words, space and
-    text = text.replace(/ a /g, " ").replace(/i feel /g, "").replace(/whats/g, "what is").replace(/please /g, "").replace(/ please/g, "");
-    let item;
-  try{
-    for(let x=0; x<trigger.length; x++){
-        for(let y=0; y<reply.length; y++){
-            if(trigger[x][y] == text){
-              console.log("ok");
-              items = reply[x];
-              item =  items[Math.floor(Math.random()*items.length)];
-              console.log("Item = "+item.toString());
-              product = item.toString();
-              return product
-            }
-            else {
-              product = alternative[Math.floor(Math.random()*alternative.length)];
-              return product
-            }
-        }
+
+  let output;
+  function proccessMessage(input){
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+    text = text
+      .replace(/ a /g, " ")
+      .replace(/i feel /g, "")
+      .replace(/whats/g, "what is")
+      .replace(/please /g, "")
+      .replace(/ please/g, "");
+
+      const match = compare(trigger, reply, text)
+    if (match) {
+      output = match;
+    } else if (text.match(/coronavirus/gi)) {
+      output = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+    } else {
+      output = alternative[Math.floor(Math.random() * alternative.length)];
     }
-  }catch{
-    product = alternative[Math.floor(Math.random()*alternative.length)];
-  }
+    return output
     
   }
+  function compare(triggerArray, replyArray, string) {
+    let item;
+    for (let x = 0; x < triggerArray.length; x++) {
+      for (let y = 0; y < replyArray.length; y++) {
+        if (triggerArray[x][y] == string) {
+          items = replyArray[x];
+          item = items[Math.floor(Math.random() * items.length)];
+        }
+      }
+    }
+    return item;
+  }
 
 
-
+  msg= req.params.msg
+  res.send("msg "+proccessMessage(msg))
     //   if(compare(trigger, reply, text)){
     //     product = compare(trigger, reply, text);
     //   } else {
@@ -123,8 +163,6 @@ app.get('/test/:msg', (req, res) => {
   //   }
   //   return item;
   // }
-  msg= req.params.msg
-  res.send(output(msg))
 })
 
 
@@ -157,70 +195,111 @@ app.get('/zoomverify/verifyzoom.html', (req, res) => {
 
 app.post('/unsplash', (req, res) => {
 /////////////////////////////////////////////////////////////////////////////////////////////
-  const trigger = [
-    ["hi", "hey", "hello"],
-    ["how are you", "how is life", "how are things"],
-    ["what are you doing", "what is going on"],
-    ["how old are you"],
-    ["who are you", "are you human", "are you bot", "are you human or bot"],
-    ["who created you", "who made you"],
-    ["your name please", "your name", "may i know your name", "what is your name"],
-    ["i love you"],
-    ["happy", "good"],
-    ["bad", "bored", "tired"],
-    ["help me", "tell me story", "tell me joke"],
-    ["ah", "yes", "ok", "okay", "nice", "thanks", "thank you"],
-    ["bye", "good bye", "goodbye", "see you later"]
-  ];
+const trigger = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["thanks", "thank you"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"]
+];
 
-  const reply = [
-    ["Hi", "Hey", "Hello"],
-    ["Fine", "Pretty well", "Fantastic"],
-    ["Nothing much", "About to go to sleep", "Can you guest?", "I don't know actually"],
-    ["I am 1 day old"],
-    ["I am just a bot", "I am a bot. What are you?"],
-    ["Buddhi Chathuranga", "My God"],
-    ["I am nameless", "I don't have a name"],
-    ["I love you too", "Me too"],
-    ["Have you ever felt bad?", "Glad to hear it"],
-    ["Why?", "Why? You shouldn't!", "Try watching TV"],
-    ["I will", "What about?"],
-    ["Tell me a story", "Tell me a joke", "Tell me about yourself", "You are welcome"],
-    ["Bye", "Goodbye", "See you later"]
-  ];
+// These are bot responses, paired in order with the above 'trigger' phrases
 
-  const alternative = ["Haha...", "Eh..."];
-  let product;
-  function output(input){
-    
-    
-    let text = (input.toLowerCase()).replace(/[^\w\s\d]/gi, ""); //remove all chars except words, space and
-    text = text.replace(/ a /g, " ").replace(/i feel /g, "").replace(/whats/g, "what is").replace(/please /g, "").replace(/ please/g, "");
+const reply = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["You're welcome"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Yes?"]
+];
 
-    let item;
+// This is a small set of basically random 'catch alls' for anything that the user enters outside of the possible trigger phrases
 
-    try{
-      for(let x=0; x<trigger.length; x++){
-          for(let y=0; y<reply.length; y++){
-              if(trigger[x][y] == text){
-                console.log("ok");
-                items = reply[x];
-                item =  items[Math.floor(Math.random()*items.length)];
-                console.log("Item = "+item.toString());
-                product = item.toString();
-                return product
-              }
-              else {
-                product = alternative[Math.floor(Math.random()*alternative.length)];
-                return product
-              }
-          }
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening..."
+];
+
+
+let output;
+function proccessMessage(input){
+  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+  text = text
+    .replace(/ a /g, " ")
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "");
+
+    const match = compare(trigger, reply, text)
+  if (match) {
+    output = match;
+  } else if (text.match(/coronavirus/gi)) {
+    output = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+  } else {
+    output = alternative[Math.floor(Math.random() * alternative.length)];
+  }
+  return output
+  
+}
+function compare(triggerArray, replyArray, string) {
+  let item;
+  for (let x = 0; x < triggerArray.length; x++) {
+    for (let y = 0; y < replyArray.length; y++) {
+      if (triggerArray[x][y] == string) {
+        items = replyArray[x];
+        item = items[Math.floor(Math.random() * items.length)];
       }
-    }catch{
-      product = alternative[Math.floor(Math.random()*alternative.length)];
     }
-      
-    }
+  }
+  return item;
+}
+
+
+msg= req.params.msg
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////
   getChatbotToken()
@@ -246,7 +325,7 @@ app.post('/unsplash', (req, res) => {
   //'text': 'You sent ' + req.body.payload.cmd + '     Replay =' + msg
   function sendChat (chatbotToken) {
     const msg = req.body.payload.cmd;
-    const replay = output(msg).toString();
+    const replay = proccessMessage(msg);
 
 
     request({
