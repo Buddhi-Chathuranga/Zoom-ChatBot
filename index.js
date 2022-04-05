cda398fe7d1a48782271edaf6e56498edbc26eca
IT19149936_Buddhi <chathuranga4lk12@gmail.com>
2022-04-05 14:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -280,6 +280,11 @@ msg= req.params.msg;
 
     const url = getSentiment(msg);
     const n = getSen(url);
+    if(msg=="Bye"){
+
+    }else{
+      User.add({message: msg});
+    }
     
     //url = "https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji-480x480.png"
     if(msg=="Bye"){
