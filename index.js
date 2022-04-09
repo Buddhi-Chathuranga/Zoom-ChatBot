f6d92486b8a9ca14d38166044423a7cb35b41f22
IT19149936_Buddhi <chathuranga4lk12@gmail.com>
2022-04-09 20:12:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,24 +30,24 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://zoomchatbot-545fd-default-rtdb.firebaseio.com"
 });
-app.use(cors( { origin:true }));
+app.use(cors({ origin: true }));
 const db = admin.firestore();
 
-app.post('/add/:msg',(req, res) => {
+app.post('/add/:msg', (req, res) => {
   const msg = req.params.msg;
   (async () => {
 
-    try{
-          await db.collection('Messages').doc()
-          .create({
-            message: msg
-          })
+    try {
+      await db.collection('Messages').doc()
+        .create({
+          message: msg
+        })
 
-          return res.status(200).send();
+      return res.status(200).send();
     }
-    catch(error){
-        console.log(error);
-        return res.status(500).send(error);
+    catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
     }
 
   })();
@@ -55,72 +55,72 @@ app.post('/add/:msg',(req, res) => {
 });
 
 
-app.get('/get',(req, res) => {
+app.get('/get', (req, res) => {
   // const snapshot = await User.get();
   // const list = snapshot.docs.map((doc) => ({ ...doc.data().message }))
   // res.send(list);
 
   (async () => {
 
-    try{
-          let query = db.collection('Messages');
-          let response = [];
+    try {
+      let query = db.collection('Messages');
+      let response = [];
 
 
-          await query.get().then(querySnapshot => {
-            let docs = querySnapshot.docs;
+      await query.get().then(querySnapshot => {
+        let docs = querySnapshot.docs;
 
-            for(let doc of docs){
-              const selectedItem = {
-                  message: doc.data().message
-              };
-              response.push(selectedItem.message);
-            };
-            return response;
-          })
-          return res.status(200).send(response);
+        for (let doc of docs) {
+          const selectedItem = {
+            message: doc.data().message
+          };
+          response.push(selectedItem.message);
+        };
+        return response;
+      })
+      return res.status(200).send(response);
     }
-    catch(error){
-        console.log(error);
-        return res.status(500).send(error);
+    catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
     }
 
   })();
-  
- 
+
+
 });
 
 
 app.get('/get', async (req, res) => {
-  
+
   (async () => {
 
-    try{
-          let query = db.collection('Messages');
-          let response = [];
+    try {
+      let query = db.collection('Messages');
+      let response = [];
 
 
-          await query.get().then(querySnapshot => {
-            let docs = querySnapshot.docs;
+      await query.get().then(querySnapshot => {
+        let docs = querySnapshot.docs;
 
-            for(let doc of docs){
-              const selectedItem = {
-                  message: doc.data().message
-              };
-              response.push(selectedItem.message);
-            };
-            return response;
-          })
-          return res.status(200).send(response);
+        for (let doc of docs) {
+          const selectedItem = {
+            message: doc.data().message
+          };
+          response.push(selectedItem.message);
+        };
+        return response;
+      })
+      return res.status(200).send(response);
     }
-    catch(error){
-        console.log(error);
-        return res.status(500).send(error);
+    catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
     }
 
   })();
-  
- 
+
+
 });
 
 
@@ -151,334 +151,234 @@ app.get('/zoomverify/verifyzoom.html', (req, res) => {
 })
 
 app.get('/test', (req, res) => {
-    
+
 })
 
 
 
 app.post('/unsplash', (req, res) => {
-//////////////////-----01------///////////////////
-const trigger = [
-  ["hi", "hey", "hello", "good morning", "good afternoon"],
-  ["how are you", "how is life", "how are things"],
-  ["what are you doing", "what is going on", "what is up"],
-  ["how old are you"],
-  ["who are you", "are you human", "are you bot", "are you human or bot"],
-  ["who created you", "who made you"],
-  [
-    "your name please",
-    "your name",
-    "may i know your name",
-    "what is your name",
-    "what call yourself"
-  ],
-  ["i love you"],
-  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
-  ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
-  ["ah", "yes", "ok", "okay", "nice"],
-  ["thanks", "thank you"],
-  ["bye", "good bye", "goodbye", "see you later"],
-  ["what should i eat today"],
-  ["bro"],
-  ["what", "why", "how", "where", "when"]
-];
+  //////////////////-----01------///////////////////
+  const trigger = [
+    ["hi", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself"
+    ],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["thanks", "thank you"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"]
+  ];
 
 
-const reply = [
-  ["Hello!", "Hi!", "Hey!", "Hi there!"],
-  [
-    "Fine... how are you?",
-    "Pretty well, how are you?",
-    "Fantastic, how are you?"
-  ],
-  [
-    "Nothing much",
-    "About to go to sleep",
-    "Can you guess?",
-    "I don't know actually"
-  ],
-  ["I am infinite"],
-  ["I am just a bot", "I am a bot. What are you?"],
-  ["The one true God, JavaScript"],
-  ["I am nameless", "I don't have a name"],
-  ["I love you too", "Me too"],
-  ["Have you ever felt bad?", "Glad to hear it"],
-  ["Why?", "Why? You shouldn't!", "Try watching TV"],
-  ["What about?", "Once upon a time..."],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
-  ["You're welcome"],
-  ["Bye", "Goodbye", "See you later"],
-  ["Sushi", "Pizza"],
-  ["Bro!"],
-  ["Yes?"]
-];
+  const reply = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!"],
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually"
+    ],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, JavaScript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["You're welcome"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Yes?"]
+  ];
 
 
-const alternative = [
-  "Same",
-  "Go on...",
-  "Bro...",
-  "Try again",
-  "I'm listening..."
-];
+  const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening..."
+  ];
 
 
-let output;
-function proccessMessage(input){
-  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
-  text = text
-    .replace(/ a /g, " ")
-    .replace(/i feel /g, "")
-    .replace(/whats/g, "what is")
-    .replace(/please /g, "")
-    .replace(/ please/g, "");
+  let output;
+  function proccessMessage(input) {
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+    text = text
+      .replace(/ a /g, " ")
+      .replace(/i feel /g, "")
+      .replace(/whats/g, "what is")
+      .replace(/please /g, "")
+      .replace(/ please/g, "");
 
     const match = compare(trigger, reply, text)
-  if (match) {
-    output = match;
-  } else if (text.match(/coronavirus/gi)) {
-    output = coronavirus[Math.floor(Math.random() * coronavirus.length)];
-  } else {
-    output = alternative[Math.floor(Math.random() * alternative.length)];
+    if (match) {
+      output = match;
+    } else if (text.match(/coronavirus/gi)) {
+      output = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+    } else {
+      output = alternative[Math.floor(Math.random() * alternative.length)];
+    }
+    return output
+
   }
-  return output
-  
-}
-function compare(triggerArray, replyArray, string) {
-  let item;
-  for (let x = 0; x < triggerArray.length; x++) {
-    for (let y = 0; y < replyArray.length; y++) {
-      if (triggerArray[x][y] == string) {
-        items = replyArray[x];
-        item = items[Math.floor(Math.random() * items.length)];
+  function compare(triggerArray, replyArray, string) {
+    let item;
+    for (let x = 0; x < triggerArray.length; x++) {
+      for (let y = 0; y < replyArray.length; y++) {
+        if (triggerArray[x][y] == string) {
+          items = replyArray[x];
+          item = items[Math.floor(Math.random() * items.length)];
+        }
       }
     }
+    return item;
   }
-  return item;
-}
-/////////////////////////////////////
+  /////////////////////////////////////
 
-///////////////----02----////////////
-function getSentiment (msg){
-  var natural = require('natural');
-  var Analyzer = natural.SentimentAnalyzer;
-  var stemmer = natural.PorterStemmer;
-  var analyzer = new Analyzer("English", stemmer, "afinn");
+  ///////////////----02----////////////
+  function getSentiment(msg) {
+    var natural = require('natural');
+    var Analyzer = natural.SentimentAnalyzer;
+    var stemmer = natural.PorterStemmer;
+    var analyzer = new Analyzer("English", stemmer, "afinn");
 
 
-  var natural = require('natural');
-  var tokenizer = new natural.WordTokenizer();
-  var trimmedText = tokenizer.tokenize(msg);
+    var natural = require('natural');
+    var tokenizer = new natural.WordTokenizer();
+    var trimmedText = tokenizer.tokenize(msg);
 
-  var k=analyzer.getSentiment(trimmedText);
-  var url;
-  if(k>0){
-    url = "https://hotemoji.com/images/dl/f/happy-emoji-by-google.png";
-    return url;
-  }
-  else if(k==0){
-    url = "https://cdn.shopify.com/s/files/1/1061/1924/products/Neutral_Face_Emoji_grande.png?v=1571606037";
-    return url;
-  }
-  else if(k<0){
-    url = "https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji-480x480.png";
-    return url;
-  }
-}
-
-function getSen (url){
-  var k;
-  if(url=="https://hotemoji.com/images/dl/f/happy-emoji-by-google.png"){
-    k = "Very Happy";
-  }
-  else if(url=="https://cdn.shopify.com/s/files/1/1061/1924/products/Neutral_Face_Emoji_grande.png?v=1571606037"){
-    k = "Happy";
-  }
-  else if(url=="https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji-480x480.png"){
-    k = "Sad";
-  }
-  else{
-
-  }
-  return k;
-}
-////////////////////////////////////
-
-msg= req.params.msg;
-
-getChatbotToken()
-
-function getChatbotToken () {
-  request({
-    url: `https://zoom.us/oauth/token?grant_type=client_credentials`,
-    method: 'POST',
-    headers: {
-      'Authorization': 'Basic ' + Buffer.from(process.env.zoom_client_id + ':' + process.env.zoom_client_secret).toString('base64')
+    var k = analyzer.getSentiment(trimmedText);
+    var url;
+    if (k > 0) {
+      url = "https://hotemoji.com/images/dl/f/happy-emoji-by-google.png";
+      return url;
     }
-  }, (error, httpResponse, body) => {
-    if (error) {
-      console.log('Error getting chatbot_token from Zoom.', error)
-    } else {
-      body = JSON.parse(body)
-      sendChat(body.access_token)
+    else if (k == 0) {
+      url = "https://cdn.shopify.com/s/files/1/1061/1924/products/Neutral_Face_Emoji_grande.png?v=1571606037";
+      return url;
     }
-  })
-}
+    else if (k < 0) {
+      url = "https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji-480x480.png";
+      return url;
+    }
+  }
 
-function sendChat (chatbotToken) {
-  request({
-    url: 'https://api.zoom.us/v2/im/chat/messages',
-    method: 'POST',
-    json: true,
-    body: {
-      'robot_jid': process.env.zoom_bot_jid,
-      'to_jid': req.body.payload.toJid,
-      'account_id': req.body.payload.accountId,
-      'content': {
-        'head': {
-          'text': 'Unsplash'
-        },
-        'body': [{
-          'type': 'message',
-          'text': 'You sent ' + req.body.payload.cmd
-        }]
+  function getSen(url) {
+    var k;
+    if (url == "https://hotemoji.com/images/dl/f/happy-emoji-by-google.png") {
+      k = "Very Happy";
+    }
+    else if (url == "https://cdn.shopify.com/s/files/1/1061/1924/products/Neutral_Face_Emoji_grande.png?v=1571606037") {
+      k = "Happy";
+    }
+    else if (url == "https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji-480x480.png") {
+      k = "Sad";
+    }
+    else {
+
+    }
+    return k;
+  }
+  ////////////////////////////////////
+
+  msg = req.params.msg;
+
+  getChatbotToken()
+
+  function getChatbotToken() {
+    request({
+      url: `https://zoom.us/oauth/token?grant_type=client_credentials`,
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(process.env.zoom_client_id + ':' + process.env.zoom_client_secret).toString('base64')
       }
-    },
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + chatbotToken
-    }
-  }, (error, httpResponse, body) => {
-    if (error) {
-      console.log('Error sending chat.', error)
-    } else {
-      console.log(body)
-    }
-  })
-}
-// getChatbotToken()
+    }, (error, httpResponse, body) => {
+      if (error) {
+        console.log('Error getting chatbot_token from Zoom.', error)
+      } else {
+        body = JSON.parse(body)
+        sendChat(body.access_token)
+      }
+    })
+  }
 
-//   function getChatbotToken () {
-//     request({
-//       url: `https://zoom.us/oauth/token?grant_type=client_credentials`,
-//       method: 'POST',
-//       headers: {
-//         'Authorization': 'Basic ' + Buffer.from(process.env.zoom_client_id + ':' + process.env.zoom_client_secret).toString('base64')
-//       }
-//     }, (error, httpResponse, body) => {
-//       if (error) {
-//         console.log('Error getting chatbot_token from Zoom.', error)
-//       } else {
-//         body = JSON.parse(body)
-//         sendChat(body.access_token)
-//       }
-//     })
-//   }
+  function sendChat(chatbotToken) {
+    const msg = req.body.payload.cmd;
+    const replay = proccessMessage(msg);
 
-//   function sendChat (chatbotToken) {
-//     const msg = req.body.payload.cmd;
-//     const replay = proccessMessage(msg);
+    const url = getSentiment(msg);
+    const n = getSen(url);
 
-//     const url = getSentiment(msg);
-//     const n = getSen(url);
-//     if(msg=="Bye"){
-      
-//     }else{
-      
-//     }
-    
-//     if(msg=="Bye"){
-//       request({
-//         url: 'https://api.zoom.us/v2/im/chat/messages',
-//         method: 'POST',
-//         json: true,
-//         body: {
-//           'robot_jid': process.env.zoom_bot_jid,
-//           'to_jid': req.body.payload.toJid,
-//           'account_id': req.body.payload.accountId,
-//           'content': {
-//             'head': {
-//               'text': 'Zoom_Bot',
-//               "style": {
-//                 "color": "#0099ff",
-//                 "bold": true,
-//                 "italic": true
-//               },
-//             },
-//                   'body': [{
-//                     "type": "section",
-//                     "sidebar_color": "#0099ff",
-//                     "sections": [
-//                         {
-//                           "type": "message",
-//                           "text": replay
-//                         }
-//                     ], 
-//                     "footer": n,
-//                     "footer_icon": url
-//                   }]
-//           }
-//         },
-//         headers: {
-//           'Content-Type': 'application/json',
-//           'Authorization': 'Bearer ' + chatbotToken
-//         }
-//       }, (error, httpResponse, body) => {
-//         if (error) {
-//           console.log('Error sending chat.', error)
-//         } else {
-//           console.log(body)
-//         }
-//       })
-//   }
-//   else{
-//     request({
-//       url: 'https://api.zoom.us/v2/im/chat/messages',
-//       method: 'POST',
-//       json: true,
-//       body: {
-//         'robot_jid': process.env.zoom_bot_jid,
-//         'to_jid': req.body.payload.toJid,
-//         'account_id': req.body.payload.accountId,
-//         'content': {
-//           'head': {
-//             'text': 'Zoom_Bot',
-//             "style": {
-//               "color": "#0099ff",
-//               "bold": true,
-//               "italic": true
-//             },
-//           },
-//                 'body': [{
-//                   "type": "section",
-//                   "sidebar_color": "#0099ff",
-//                   "sections": [
-//                       {
-//                         "type": "message",
-//                         "text": replay
-//                       }
-//                   ], 
-//                   "footer": ""
-//                 }]
-//         }
-//       },
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer ' + chatbotToken
-//       }
-//     }, (error, httpResponse, body) => {
-//       if (error) {
-//         console.log('Error sending chat.', error)
-//       } else {
-//         console.log(body)
-//       }
-//     })
-  
-//   }
-// }
 
-  
+    request({
+      url: 'https://api.zoom.us/v2/im/chat/messages',
+      method: 'POST',
+      json: true,
+      body: {
+        'robot_jid': process.env.zoom_bot_jid,
+        'to_jid': req.body.payload.toJid,
+        'account_id': req.body.payload.accountId,
+        'content': {
+          'head': {
+            'text': 'Zoom_Bot',
+            "style": {
+              "color": "#0099ff",
+              "bold": true,
+              "italic": true
+            },
+          },
+          'body': [{
+            "type": "section",
+            "sidebar_color": "#0099ff",
+            "sections": [
+              {
+                "type": "message",
+                "text": msg
+              }
+            ],
+            "footer": n,
+            "footer_icon": url
+          }]
+        }
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + chatbotToken
+      }
+    }, (error, httpResponse, body) => {
+      if (error) {
+        console.log('Error sending chat.', error)
+      } else {
+        console.log(body)
+      }
+    })
+  }
+
+
 })
 
 app.post('/deauthorize', (req, res) => {
