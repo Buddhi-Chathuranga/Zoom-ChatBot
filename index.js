a0c9c6020ac511d899c8ae960c3eaaffd4de1f14
IT19149936_Buddhi <chathuranga4lk12@gmail.com>
2022-04-10 11:47:31 +0530

    install natural

--- index.js
+++ index.js
@@ -74,113 +74,159 @@ app.get('/zoomverify/verifyzoom.html', (req, res) => {
 
 
 app.post('/unsplash', (req, res) => {
-/////////////////////////////////////
-const trigger = [
-  ["hi", "hey", "hello", "good morning", "good afternoon"],
-  ["how are you", "how is life", "how are things"],
-  ["what are you doing", "what is going on", "what is up"],
-  ["how old are you"],
-  ["who are you", "are you human", "are you bot", "are you human or bot"],
-  ["who created you", "who made you"],
-  [
-    "your name please",
-    "your name",
-    "may i know your name",
-    "what is your name",
-    "what call yourself"
-  ],
-  ["i love you"],
-  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
-  ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
-  ["ah", "yes", "ok", "okay", "nice"],
-  ["thanks", "thank you"],
-  ["bye", "good bye", "goodbye", "see you later"],
-  ["what should i eat today"],
-  ["bro"],
-  ["what", "why", "how", "where", "when"]
-];
+  /////////////////////////////////////
+  const trigger = [
+    ["hi", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself"
+    ],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["thanks", "thank you"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"]
+  ];
 
 
-const reply = [
-  ["Hello!", "Hi!", "Hey!", "Hi there!"],
-  [
-    "Fine... how are you?",
-    "Pretty well, how are you?",
-    "Fantastic, how are you?"
-  ],
-  [
-    "Nothing much",
-    "About to go to sleep",
-    "Can you guess?",
-    "I don't know actually"
-  ],
-  ["I am infinite"],
-  ["I am just a bot", "I am a bot. What are you?"],
-  ["The one true God, JavaScript"],
-  ["I am nameless", "I don't have a name"],
-  ["I love you too", "Me too"],
-  ["Have you ever felt bad?", "Glad to hear it"],
-  ["Why?", "Why? You shouldn't!", "Try watching TV"],
-  ["What about?", "Once upon a time..."],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
-  ["You're welcome"],
-  ["Bye", "Goodbye", "See you later"],
-  ["Sushi", "Pizza"],
-  ["Bro!"],
-  ["Yes?"]
-];
+  const reply = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!"],
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually"
+    ],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, JavaScript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["You're welcome"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Yes?"]
+  ];
 
 
-const alternative = [
-  "Same",
-  "Go on...",
-  "Bro...",
-  "Try again",
-  "I'm listening..."
-];
+  const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening..."
+  ];
 
 
-let output;
-function proccessMessage(input){
-  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
-  text = text
-    .replace(/ a /g, " ")
-    .replace(/i feel /g, "")
-    .replace(/whats/g, "what is")
-    .replace(/please /g, "")
-    .replace(/ please/g, "");
+  let output;
+  function proccessMessage(input) {
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+    text = text
+      .replace(/ a /g, " ")
+      .replace(/i feel /g, "")
+      .replace(/whats/g, "what is")
+      .replace(/please /g, "")
+      .replace(/ please/g, "");
 
     const match = compare(trigger, reply, text)
-  if (match) {
-    output = match;
-  } else if (text.match(/coronavirus/gi)) {
-    output = coronavirus[Math.floor(Math.random() * coronavirus.length)];
-  } else {
-    output = alternative[Math.floor(Math.random() * alternative.length)];
+    if (match) {
+      output = match;
+    } else if (text.match(/coronavirus/gi)) {
+      output = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+    } else {
+      output = alternative[Math.floor(Math.random() * alternative.length)];
+    }
+    return output
+
   }
-  return output
-  
-}
-function compare(triggerArray, replyArray, string) {
-  let item;
-  for (let x = 0; x < triggerArray.length; x++) {
-    for (let y = 0; y < replyArray.length; y++) {
-      if (triggerArray[x][y] == string) {
-        items = replyArray[x];
-        item = items[Math.floor(Math.random() * items.length)];
+  function compare(triggerArray, replyArray, string) {
+    let item;
+    for (let x = 0; x < triggerArray.length; x++) {
+      for (let y = 0; y < replyArray.length; y++) {
+        if (triggerArray[x][y] == string) {
+          items = replyArray[x];
+          item = items[Math.floor(Math.random() * items.length)];
+        }
       }
     }
+    return item;
   }
-  return item;
-}
 
-msg= req.params.msg
+  msg = req.params.msg
 
-///////////////////////////////////////////
+  /////////////////////////////////////
+
+  ///////////////----02----////////////
+  function getSentiment(msg) {
+    var natural = require('natural');
+    var Analyzer = natural.SentimentAnalyzer;
+    var stemmer = natural.PorterStemmer;
+    var analyzer = new Analyzer("English", stemmer, "afinn");
+
+
+    var natural = require('natural');
+    var tokenizer = new natural.WordTokenizer();
+    var trimmedText = tokenizer.tokenize(msg);
+
+    var k = analyzer.getSentiment(trimmedText);
+    var url;
+    if (k > 0) {
+      url = "https://hotemoji.com/images/dl/f/happy-emoji-by-google.png";
+      return url;
+    }
+    else if (k == 0) {
+      url = "https://cdn.shopify.com/s/files/1/1061/1924/products/Neutral_Face_Emoji_grande.png?v=1571606037";
+      return url;
+    }
+    else if (k < 0) {
+      url = "https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji-480x480.png";
+      return url;
+    }
+  }
+
+  function getSen(url) {
+    var k;
+    if (url == "https://hotemoji.com/images/dl/f/happy-emoji-by-google.png") {
+      k = "Very Happy";
+    }
+    else if (url == "https://cdn.shopify.com/s/files/1/1061/1924/products/Neutral_Face_Emoji_grande.png?v=1571606037") {
+      k = "Happy";
+    }
+    else if (url == "https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji-480x480.png") {
+      k = "Sad";
+    }
+    else {
+
+    }
+    return k;
+  }
+  ////////////////////////////////////
   getChatbotToken()
 
-  function getChatbotToken () {
+  function getChatbotToken() {
     request({
       url: `https://zoom.us/oauth/token?grant_type=client_credentials`,
       method: 'POST',
@@ -196,31 +242,65 @@ msg= req.params.msg
       }
     })
   }
-  
-  function sendChat (chatbotToken) {
+
+  function sendChat(chatbotToken) {
     const msg = req.body.payload.cmd;
     const replay = proccessMessage(msg);
 
+    let fullChat = [];
 
-    if(!(msg=="Bye" || msg=="bye")){
+    if (!(msg == "Bye" || msg == "bye")) {
       (async () => {
 
         try {
-          await db.collection('Messages').doc()
-            .create({
-              message: msg
-            })
-    
-          return res.status(200).send();
+          let query = db.collection('Messages');
+
+          await query.get().then(querySnapshot => {
+            let docs = querySnapshot.docs;
+
+            for (let doc of docs) {
+              const selectedItem = {
+                message: doc.data().message
+              };
+              fullChat.push(selectedItem.message);
+            };
+            return fullChat;
+          })
+          return res.status(200).send(fullChat.join(". "));
         }
         catch (error) {
           console.log(error);
           return res.status(500).send(error);
         }
-    
-      })();
-    }else{
 
+      })();
+    } else {
+      (async () => {
+
+        try {
+          let query = db.collection('Messages');
+
+
+
+          await query.get().then(querySnapshot => {
+            let docs = querySnapshot.docs;
+
+            for (let doc of docs) {
+              const selectedItem = {
+                message: doc.data().message
+              };
+              response.push(selectedItem.message);
+            };
+            return response;
+          })
+          return res.status(200).send(response);
+        }
+        catch (error) {
+          console.log(error);
+          return res.status(500).send(error);
+        }
+
+      })();
     }
 
 
@@ -239,7 +319,7 @@ msg= req.params.msg
           },
           'body': [{
             'type': 'message',
-            'text':  replay
+            'text': replay
           }]
         }
       },
@@ -256,7 +336,7 @@ msg= req.params.msg
     })
   }
 
-  
+
 })
 
 app.post('/deauthorize', (req, res) => {
